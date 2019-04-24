1ae72291299652fd32487c86bd5c8fbe2e7116d7
Aron de Looijer <aron@mbbma.nl>
2019-04-24 15:14:51 +0200

    [ADD] GetUrlParameter functie in JS

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -288,6 +288,25 @@ jQuery(document).ready(function($) {
 	});
 
 	/***************************
+	Get parameter from URL
+	***************************/
+	let getUrlParameter = function getUrlParameter(sParam) {
+		let sPageURL = window.location.search.substring(1),
+			sURLVariables = sPageURL.split('&'),
+			sParameterName,
+			i;
+	
+		for (i = 0; i < sURLVariables.length; i++) {
+			sParameterName = sURLVariables[i].split('=');
+	
+			if (sParameterName[0] === sParam) {
+				return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+			}
+		}
+	};
+	// var tech = getUrlParameter('technology');
+
+	/***************************
 	Gravity forms
 	***************************/
 	checkForm();
